ecfc7b03793cae6aa35d1ec8d2e22a3b11151671
Brian <briandika.mankin@gmail.com>
2025-04-19 12:00:15 +0700

    feat: add more styling and some transitions

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,30 +1,27 @@
 export const About = () => {
   return (
-    <>
-      <div>
-        <div className="par1">
-          I’m a Software Engineer focused on building efficient, user-centric
-          solutions with clean, scalable code. While I enjoy working across the
-          stack, I’m especially drawn to backend engineering and app
-          accessibility — I believe the best software is software that everyone
-          can use.
-        </div>
-        <div className="par2">
-          Currently, I’m a Junior Software Engineer, contributing to the
-          development of full-stack web applications and refining user
-          experiences with performance and accessibility in mind.
-        </div>
-        <div className="par3">
-          In the past, I had the opportunity to develop and maintain web-based
-          ERP systems for a range of clients, from startups to large retail
-          companies. I also worked directly with clients, collaborating closely
-          to tailor solutions that met their business needs.
-        </div>
-        <div className="par4">
-          Outside of work, I love going on long motorbike trips and exploring
-          new places — the road helps me reset and refuel for what’s next.
-        </div>
-      </div>
-    </>
+    <section id="about" aria-label="about me" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+      <p className="mb-4">
+        I’m a Software Engineer focused on building efficient, user-centric
+        solutions with clean, scalable code. While I enjoy working across the
+        stack, I’m especially drawn to backend engineering and app accessibility
+        — I believe the best software is software that everyone can use.
+      </p>
+      <p className="mb-4">
+        Currently, I’m a Junior Software Engineer, contributing to the
+        development of full-stack web applications and refining user experiences
+        with performance and accessibility in mind.
+      </p>
+      <p className="mb-4">
+        In the past, I had the opportunity to develop and maintain web-based ERP
+        systems for a range of clients, from startups to large retail companies.
+        I also worked directly with clients, collaborating closely to tailor
+        solutions that met their business needs.
+      </p>
+      <p className="mb-4">
+        Outside of work, I love going on long motorbike trips and exploring new
+        places — the road helps me reset and refuel for what’s next.
+      </p>
+    </section>
   );
 };
